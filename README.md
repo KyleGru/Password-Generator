aa647ad3847858bcf4da4b40eca723bd81573966
KyleGru <136273960+KyleGru@users.noreply.github.com>
2023-11-04 17:13:11 -0400

    adjusted the generatePassword function, added .split method to global scope variables, fixed typos

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 ## Resources
 https://www.youtube.com/watch?v=iKo9pDKKHnc
 https://www.javatpoint.com/java-do-while-loop
+https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
 
 ## Links
 
--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var specialChar = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split('');
+var number = '0123456789'.split('');
+var specialChar = '@%+/\'!#$^?:,)(}{][~_.'.split('');
 
 function prompts() {
   var valid = false;
   do {
     var length = prompt("Please enter how long you want your password to be (between 8 and 128).");
-    var upperCase = confirm("Press 'OK' if you want uppercase letters.");
-    var lowerCase = confirm("Press 'OK' if you want lowercase letters.")
-    var number = confirm("Press 'OK' if you want numbers.");
-    var specialChar = confirm("Press 'OK' if you want special characters.");
+    var upperCasePromt = confirm("Press 'OK' if you want uppercase letters.");
+    var lowerCasePromt = confirm("Press 'OK' if you want lowercase letters.")
+    var numberPromt = confirm("Press 'OK' if you want numbers.");
+    var specialCharPromt = confirm("Press 'OK' if you want special characters.");
     var alerts = {
       length: length,
-      upperCase: upperCase,
-      lowerCase: lowerCase,
-      number: number,
-      specialChar: specialChar
+      upperCasePromt: upperCasePromt,
+      lowerCasePromt: lowerCasePromt,
+      numberPromt: numberPromt,
+      specialCharPromt: specialCharPromt
     }
     if ((length < 8 || length > 128))
     alert("Make sure you choose a password between 8 and 128 characters.");
-    else if((!upperCase)&&(!lowerCase)&&(!number)&&(!specialChar))
+    else if((!upperCasePromt)&&(!lowerCasePromt)&&(!numberPromt)&&(!specialCharPromt))
     alert("Need to select at least one of the given promts.");
   else valid = true;
   
@@ -33,32 +33,34 @@ return alerts;
 
 function generatePassword() {
   var passwordSelctions = prompts();
-  var combonations = [];
-  var password = "";
+  var combinations = [];
+  var validPassword = "";
 
-  if (passwordSelctions.upperCase === true) {
-    combonations.push(upperCase);
+  if (passwordSelctions.upperCasePromt) {
+    for (var i of upperCase)
+    combinations.push(i);
   }
-  if (passwordSelctions.lowerCase === true) {
-    combonations.push(lowerCase);
+  if (passwordSelctions.lowerCasePromt) {
+    for (var i of lowerCase)
+    combinations.push(i);
   }
-  if (passwordSelctions.number === true) {
-    combonations.push(number);
+  if (passwordSelctions.numberPromt) {
+    for (var i of number)
+    combinations.push(i);
   }
-  if (passwordSelctions.specialChar === true) {
-    combonations.push(specialChar);
+  if (passwordSelctions.specialCharPromt) {
+    for (var i of specialChar)
+    combinations.push(i);
   }
 
-  
-  
-  console.log(combonations);
+  console.log(combinations);
 
   for (var i = 0; i < passwordSelctions.length; i++) {
-    password += combonations[Math.floor(Math.random() + combonations.length)];
+    validPassword += combinations[Math.floor(Math.random() * combinations.length)];
   }
 
-  console.log(password);
-  return password;
+  console.log(validPassword);
+  return validPassword;
 }
 
 
